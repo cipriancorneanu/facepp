226a94524c01de9d02f732c911982a157087bb6c
CiprianCorneanu <cipriancorneanu@gmail.com>
2017-06-15 11:14:34 +0200

    Changed writing data to disk from pickle to hdf5. Added shuffling to augmented data in GeneratorFera2017.

--- reader/generator.py
+++ reader/generator.py
@@ -1,9 +1,10 @@
 __author__ = 'cipriancorneanu'
 
 import cPickle
+import h5py
 import numpy as np
 import gc
-from skimage.transform import resize
+from sklearn.utils import shuffle
 from imgaug.imgaug import augmenters as iaa
 
 class GeneratorFera2017():
@@ -13,6 +14,7 @@ class GeneratorFera2017():
         self.n_batches_train = nbt
         self.n_batches_validation = nbv
 
+    '''
     def generate(self, datagen, mini_batch_size=32):
         mega_batch = 0
         #Iterate through mega_batches infinitely
@@ -20,6 +22,25 @@ class GeneratorFera2017():
             # Load mega batch from file
             fera = cPickle.load(open(self.path+'train/fera17_train_' + str(mega_batch%self.n_batches_train), 'rb'))
             x, y = (fera['ims'], fera['occ'])
+
+            if self.data_format == 'channels_first':
+                x = np.rollaxis(x, 3, 1)
+
+            # Yield as many mini_batches as it fits the mega_batch_size
+            for i, (x_batch, y_batch) in enumerate(datagen.flow(x, y, batch_size=mini_batch_size)):
+                if i==x.shape[0]//mini_batch_size: break
+                yield (x_batch, y_batch)
+
+            mega_batch += 1
+            gc.collect()
+    '''
+
+    def generate(self, datagen, mini_batch_size=32):
+        mega_batch = 0
+        #Iterate through mega_batches infinitely
+        while True:
+            with h5py.File(self.path+'train/fera17_train_aug_' + str(mega_batch%self.n_batches_train)+'.h5', 'r') as hf:
+                x, y = (hf['dt']['ims'][()], hf['dt']['occ'][()])
 
             if self.data_format == 'channels_first':
                 x = np.rollaxis(x, 3, 1)
@@ -59,6 +80,15 @@ class GeneratorFera2017():
             dt = cPickle.load(open(self.path+'train/'+'fera17_train_' + str(bat), 'rb'))
             x_train.append(dt['ims'])
             y_train.append(dt['occ'])
+
+        return (np.concatenate(x_train), np.concatenate(y_train))
+
+    def load_augmented_train(self):
+        x_train, y_train = ([], [])
+        for bat in range(self.n_batches_train):
+            with h5py.File(self.path+'train/'+'fera17_train_aug_'+str(bat)+'.h5', 'r') as hf:
+                x_train.append(hf['dt']['ims'][()])
+                y_train.append(hf['dt']['occ'][()])
 
         return (np.concatenate(x_train), np.concatenate(y_train))
 
@@ -113,33 +143,43 @@ class GeneratorFera2017():
             return np.exp(x) / np.sum(np.exp(x), axis=0)
 
         # Apply augmentation
-        for mega_batch in range(self.n_batches_train):
+        for mega_batch in range(1):
             print('Loading mega batch {}'.format(mega_batch))
             dt = cPickle.load(open(self.path+'train/'+'fera17_train_' + str(mega_batch), 'rb'))
 
             print('Augmenting mega batch {}'.format(mega_batch))
 
+            # Compute pdf for selacting observations to augment. Higher probability to minority classes.
             pdf = softmax([8*max(0,0.5-x) for x in [0.20, 0.19, 0.45, 0.56, 0.59, 0.56, 0.48, 0.16, 0.35, 0.16]])
             positives = [np.where(dt['occ'][:,au]==1)[0] for au in range(10)]
 
+            # Compute indices of observations to augment
             n_classes, n_choices  = 10, 100
             aus = np.random.choice(n_classes, n_choices, p=pdf)
-            aug_idx = np.concatenate([np.random.choice(positives[au], .5*dt['occ'].shape[0]/n_choices) for au in aus if positives[au].size])
+            aug_idx = np.concatenate([np.random.choice(positives[au], dt['occ'].shape[0]/n_choices) for au in aus if positives[au].size])
 
             # Augment selected images
             aug_ims = seq.augment_images(dt['ims'][aug_idx])
 
-            # Recreate data
-            dt['ims'] = np.concatenate((dt['ims'], aug_ims))
-            dt['occ'] = np.concatenate((dt['occ'], dt['occ'][aug_idx]))
-            dt['int'] = np.concatenate((dt['int'], dt['int'][aug_idx]))
-            dt['poses'] = np.concatenate((dt['poses'], dt['poses'][aug_idx]))
-            dt['subjects'] = np.concatenate((dt['subjects'], dt['subjects'][aug_idx]))
-            dt['tasks'] = np.concatenate((dt['tasks'], dt['tasks'][aug_idx]))
-            dt['geoms'] = np.concatenate((dt['geoms'], dt['geoms'][aug_idx]))
+            # Concatenate augmented and shuffle
+            dt['ims'], dt['occ'], dt['int'], dt['poses'], dt['subjects'], dt['tasks'], dt['geoms'] = shuffle(
+                np.concatenate((dt['ims'], aug_ims)),
+                np.concatenate((dt['occ'], dt['occ'][aug_idx])),
+                np.concatenate((dt['int'], dt['int'][aug_idx])),
+                np.concatenate((dt['poses'], dt['poses'][aug_idx])),
+                np.concatenate((dt['subjects'], dt['subjects'][aug_idx])),
+                np.concatenate((dt['tasks'], dt['tasks'][aug_idx])),
+                np.concatenate((dt['geoms'], dt['geoms'][aug_idx])),
+            )
 
             print('Dumping augmented mega batch {}'.format(mega_batch))
-            cPickle.dump(dt, open(self.path+'train/fera17_train_aug_' + str(mega_batch), 'wb'), cPickle.HIGHEST_PROTOCOL)
+            file = h5py.File(self.path+'train/fera17_train_aug_' + str(mega_batch)+'.h5', 'w')
+            grp = file.create_group('dt')
+
+            for k,v in dt.items():
+                grp.create_dataset(k, data=v)
+
+            #cPickle.dump(dt, open(self.path+'train/fera17_train_aug_' + str(mega_batch), 'wb'), cPickle.HIGHEST_PROTOCOL)
 
 class GeneratorCKPlus():
     def __init__(self, path, n_channels=1, data_format='channels_last'):
@@ -221,14 +261,13 @@ class GeneratorMLMNIST():
             return np.rollaxis(self.x_train, 3, 1)
 
 if __name__ == '__main__':
-    path = '/home/corneanu/data/fera2017/'
-    dtg = GeneratorFera2017(path)
-    '''
+    path_server = '/home/corneanu/data/fera2017/'
+    path_local = '/Users/cipriancorneanu/Research/data/fera2017/'
+    dtg = GeneratorFera2017(path_local)
     dtg.augment()
-    '''
 
-    print(dtg.class_imbalance())
-    
+    #dtg.load_augmented_train()
+
     '''
     import matplotlib.pyplot as plt
 
