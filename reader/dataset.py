73794162a89404889be1b0142a44ab749980fac0
CiprianCorneanu <cipriancorneanu@gmail.com>
2017-10-17 10:25:00 +0200

    Changed augmentation function to make less intense for BP4D.

--- reader/dataset.py
+++ reader/dataset.py
@@ -323,56 +323,6 @@ class ReaderFera2017():
                 segment.create_dataset('poses', data=poses[x])
                 segment.create_dataset('tasks', data=tasks[x])
 
-    '''
-    def prepare(self, partition, pose, out_fname):
-        path = self.path+partition+'/aligned/'+pose+'/'
-        files_fera17 = sorted([f for f in os.listdir(path)])
-
-        # Load train data from aligned
-        ims, lms, aus, ints, subjects, tasks, poses = ([], [], [], [], [], [], [])
-        for fname in files_fera17:
-            print 'Loading file {}'.format(fname)
-            dt = cPickle.load(open(path+fname, 'rb'))
-
-            ims.append(dt['ims'])
-            lms.append(dt['geoms'])
-            aus.append(dt['occ'])
-            ints.append(dt['int'])
-            subjects.append(dt['subjects']*np.concatenate(dt['ims']).shape[0])
-            poses.append(dt['poses']*np.concatenate(dt['ims']).shape[0])
-            tasks.append(dt['tasks']*np.concatenate(dt['ims']).shape[0])
-
-
-
-        (ims, lms, aus, ints, subjects, poses, tasks) = (np.squeeze(np.concatenate(ims, axis=1)), np.squeeze(np.concatenate(lms, axis=1)), \
-                                           np.squeeze(np.concatenate(aus, axis=1)), np.squeeze(np.concatenate(ints, axis=1)),
-                                           np.concatenate(subjects), np.concatenate(poses), np.concatenate(tasks))
-
-        print 'Total number of samples is {}'.format(ims.shape)
-
-        # Shuffle and split
-        idx = np.random.permutation(ims.shape[0])
-        slice_length = 1024
-        indices = [slice_length*x for x in range(1,int(np.ceil(ims.shape[0]/slice_length+1)))]
-        splits = np.array_split(idx, indices)
-
-        # Dump data
-        if os.path.isfile(self.path+out_fname):
-            file = h5py.File(self.path+out_fname, 'r+')
-        else:
-            file = h5py.File(self.path+out_fname, 'w')
-
-        grp = file.create_group(partition+'/'+pose+'/')
-        for i,x in enumerate(splits):
-            segment = grp.create_group('segment_'+str(i))
-            segment.create_dataset('faces', data=ims[x])
-            segment.create_dataset('lms', data=lms[x])
-            segment.create_dataset('aus', data=aus[x])
-            segment.create_dataset('subjects', data=subjects[x])
-            segment.create_dataset('poses', data=poses[x])
-            segment.create_dataset('tasks', data=tasks[x])
-    '''
-
     def prepare_patched_faces(self, partition, pose, out_fname, verbose=False):
         print 'Prepare patched faces for database {}'.format(out_fname)
         with h5py.File(self.path+out_fname, 'r+') as hf:
@@ -446,46 +396,18 @@ class ReaderFera2017():
                 #iaa.Flipud(0.2), # vertically flip 20% of all images
                 sometimes(iaa.Crop(percent=(0, 0.1))), # crop images by 0-10% of their height/width
                 sometimes(iaa.Affine(
-                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
-                    translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-                    rotate=(-45, 45), # rotate by -45 to +45 degrees
+                    scale={"x": (0.9, 1.1), "y": (0.9, 1.1)}, # scale images to 80-120% of their size, individually per axis
+                    translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)}, # translate by -20 to +20 percent (per axis)
+                    rotate=(-20, 20), # rotate by -45 to +45 degrees
                     shear=(-16, 16), # shear by -16 to +16 degrees
                     order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
                     cval=(0, 255), # if mode is constant, use a cval between 0 and 255
                     mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
                 )),
-                # execute 0 to 5 of the following (less important) augmenters per image
-                # don't execute all of them, as that would often be way too strong
-                iaa.SomeOf((0, 5),
-                    [
-                        sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
-                        iaa.OneOf([
-                            iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
-                            iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
-                            iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
-                        ]),
-                        iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
-                        iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
-                        # search either for all edges or for directed edges
-                        sometimes(iaa.OneOf([
-                            iaa.EdgeDetect(alpha=(0, 0.7)),
-                            iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
-                        ])),
-                        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
-                        iaa.OneOf([
-                            iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
-                            iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
-                        ]),
-                        iaa.Invert(0.05, per_channel=True), # invert color channels
-                        iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
-                        iaa.Multiply((0.5, 1.5), per_channel=0.5), # change brightness of images (50-150% of original value)
-                        iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
-                        iaa.Grayscale(alpha=(0.0, 1.0)),
-                        sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-                        sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))) # sometimes move parts of the image around
-                    ],
-                    random_order=True
-                )
+
+                sometimes(
+                    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5) # add gaussian noise to images
+                ),
             ],
             random_order=True
         )
